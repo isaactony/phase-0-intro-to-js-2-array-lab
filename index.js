ed074e6d078080a7a618302dea976c836c25e51c
isaac <isaactonyloi@gmail.com>
2022-11-28 16:00:14 +0300

    javascript arrays

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+   let newArray = [...cats, "Broom"];
+   return newArray;
+
+}
+//appendCat();
+function prependCat() {
+    let newArray = ["Arnold", ...cats];
+    return newArray;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
